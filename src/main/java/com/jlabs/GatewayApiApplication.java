afd6475df88c97eb46583b1c2a7ba45a4ec96220
smita <smita.s.kothari@gmail.com>
2016-11-28 21:03:51 -0600

    adding zuulproxy annotation

--- src/main/java/com/jlabs/GatewayApiApplication.java
+++ src/main/java/com/jlabs/GatewayApiApplication.java
@@ -2,11 +2,14 @@ package com.jlabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApiApplication.class, args);
+
 	}
 }
